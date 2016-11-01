f4800a27170bfe0ece300203b70a07a01912fe8c
Steven Almeroth <sroth77@gmail.com>
2016-10-31 19:15:00 -0700

    load

--- app/timelib.js
+++ app/timelib.js
@@ -6,13 +6,143 @@
 })(this, function(){
     "use strict"
 
+    function load_years (){
+        var
+            _day_width = timelion.config.year_width/12/30,
+            _years = timelion.data.map(function(d){return d.date[0]}),
+            _;
+
+        timelion.years = {};
+        timelion.first_year = Math.min.apply(null, _years);
+        timelion.last_year = Math.max.apply(null, _years);
+
+        for (
+            var year = timelion.first_year, age = 0;
+            year <= timelion.last_year + 1;
+            year++, age++
+        ){
+            var days = (year % 4 == 0) ? 366 : 365; // leap years
+            timelion.years[year] = {
+                age: age,
+                days: days,
+                width: (days*_day_width)
+            }
+        }
+    }
+
+    function load_event ( d ){
+        var firstYear = timelion.first_year;
+        var yearLength = timelion.config.year_width;
+        var monthLength = yearLength/12;
+        var dayLength = monthLength/30;
+
+        var startYear = d.date[0];
+        var startMonth = d.date[1];
+        var startDate = d.date[2];
+        var endYear = undefined;
+        var endMonth = undefined;
+        var endDate = undefined;
+        var width = 0;
+
+        // Calculate offset
+        var startTime = new Date(firstYear, 0, 1);
+        var endTime = new Date(startYear, startMonth ? startMonth-1 : 0, startDate || 1);
+        var daysDiff = (endTime - startTime)/(24*60*60*1000);
+        d.offset = daysDiff*dayLength;
+
+        // Calculate width
+        if (endYear){
+            var _endMonth = endMonth ? endMonth-1 : 11;
+            var _endDate = endDate || new Date(endYear, _endMonth+1, 0).getDate();
+            startTime = new Date(startYear, startMonth ? startMonth-1 : 0, startDate || 1);
+            endTime = new Date(endYear, _endMonth, _endDate);
+            daysDiff = (endTime - startTime)/(24*60*60*1000);
+            width = daysDiff*dayLength;
+        } else {
+            if (startDate){
+                width = dayLength;
+            } else if (startMonth){
+                width = monthLength;
+            } else {
+                width = yearLength;
+            }
+        }
+        d.width = width
+    }
+
+    function load_events (){
+        timelion.data.forEach(function(d){
+            load_event( d )
+        });
+    }
+
+    function load ( data ){
+        timelion.data = data.data;
+        load_years()
+        load_events()
+        timelion.loaded = true
+    }
+
     return {
 
-        loaded: false,
+        data: undefined,
+        loaded: undefined,
+        rendered: undefined,
+        last_year: undefined,
+        first_year: undefined,
+
+        config: {
+            year_width: 120, // pixels
+            show_age: true   // show age on year axis
+        },
+
+        init: function(){
+            timelion.reset()
+        },
+
+        reset: function(){
+            timelion.data = null;
+            timelion.loaded = false;
+            timelion.rendered = false;
+            timelion.last_year = 0;
+            timelion.first_year = 0;
+        },
 
         load: function( filename ){
-            timelion.loaded = true;
-            return {}
+            return new Promise(function( resolve, reject ) {
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', filename, true);
+                xhr.onload = function(){
+                    var data;
+                    if (xhr.status == 200){
+                        try{
+                            data = JSON.parse(xhr.responseText);
+                        }
+                        catch (e){
+                            reject('File is not JSON ('+xhr.responseText+')');
+                        }
+                        if ('data' in data){
+                            resolve(load( data ))
+                        }
+                        else
+                            reject('File contains no "data" ('+xhr.status+')')
+                    }
+                    else
+                        reject('Could not load ('+filename+') status ('+xhr.status+')')
+                };
+                xhr.send();
+            });
+        },
+
+        render: function( filename ){
+            return new Promise(function( resolve, reject ) {
+                if ( !timelion.loaded ){
+                    reject('Timelion not loaded, run load first');
+                    return
+                }
+                timelion.rendered = true;
+                resolve()
+            });
         }
     }
 })
--- tests/spec/TimelionSpec.js
+++ tests/spec/TimelionSpec.js
@@ -1,44 +1,108 @@
 describe("timelion", function() {
-  var
-    timelion = window.timelion;
-
-  it("should be an object", function() {
-    expect(typeof timelion).toEqual('object');
-  });
-
-  it("should not be loaded", function() {
-    expect(timelion.loaded).toBe(false);
-  });
-
-  describe("load", function() {
     var
-      timelion = window.timelion,
-      timelion_data_filename = '../app/example.json',
-      data,
-      _;
+        timelion = window.timelion,
+        timelion_data_filename = '../app/example.json',
+        _;
 
-    beforeEach(function(done) {
-      setTimeout(function() {
-        data = timelion.load( timelion_data_filename );
-        done();
-      }, 1);
+    it("should be an object", function() {
+        expect(typeof timelion).toEqual('object');
+    });
+    it("should have a valid configuration", function() {
+        expect(typeof timelion.config).toEqual('object');
+        expect(typeof timelion.config.show_age).toEqual('boolean');
+        expect(timelion.config.year_width).toBeGreaterThan(0);
+    });
+    timelion.init();
+    it("should not be loaded", function() {
+        expect(timelion.loaded).toBe(false);
+        // expect(timelion.html).toEqual('');
     });
 
-    it("should be loaded", function() {
-      expect(timelion.loaded).toBe(true);
+    describe("data", function() {
+
+        beforeAll(function(done) {
+            timelion.reset();
+            setTimeout(function() {
+                timelion.load( timelion_data_filename )
+                .then(function(response) { done() })
+                .catch(function(error) { console.log(error); done() })
+            }, 1);
+        });
+
+        it("should be loaded", function() {
+            expect(timelion.loaded).toBe(true);
+        });
+        it("should contain at least one record", function() {
+            expect(typeof timelion.data).toEqual('object');
+            expect(timelion.data.length).toBeGreaterThan(0);
+            timelion.data.forEach(function(event){});
+        });
+        it("should contain valid events", function() {
+            timelion.data.forEach(function(event){
+                expect(event.date.length).toEqual(3);
+                expect(typeof event.date[0]).toEqual('number');
+                expect(typeof event.date[1]).toEqual('number');
+                expect(typeof event.date[2]).toEqual('number');
+                expect(typeof event.offset).toEqual('number');
+                expect(typeof event.width).toEqual('number');
+                expect(typeof event.title).toEqual('string');
+            });
+        });
+        it("should have valid years", function() {
+            expect(typeof timelion.first_year).toEqual('number');
+            expect(typeof timelion.last_year).toEqual('number');
+            expect(timelion.first_year).toBeGreaterThan(0);
+            expect(timelion.last_year).toBeGreaterThan(0);
+            expect(timelion.last_year).toBeGreaterThanOrEqual(timelion.first_year);
+            expect(typeof timelion.years[timelion.first_year]).toEqual('object');
+        });
     });
 
-    describe("file name", function() {
-      it("should be a string", function() {
-        expect(typeof timelion_data_filename).toEqual('string');
-      });
-    });
+    describe("render", function() {
 
-    describe("file data", function() {
-      it("should be an object", function() {
-        expect(typeof data).toEqual('object');
-      });
+        it("should not be rendered", function() {
+            expect(timelion.rendered).toBe(false);
+        });
+
+        describe("unloaded", function() {
+
+            beforeAll(function(done) {
+                timelion.reset();
+                setTimeout(function() {
+                    timelion.render().then(function(response) {
+                      done();
+                    }, function(error) {
+                      done();
+                    })
+                }, 1);
+            });
+
+            it("should not be rendered", function() {
+                expect(timelion.rendered).toBe(false);
+            });
+        });
+
+        describe("loaded", function() {
+
+            beforeAll(function(done) {
+                timelion.reset();
+                setTimeout(function() {
+                    timelion.load( timelion_data_filename )
+                    .then(function(response) {
+                        timelion.render().then(function(response) {
+                          done();
+                        }, function(error) {
+                          done();
+                        })
+                    });
+                }, 1);
+            });
+
+            it("should be rendered", function() {
+                expect(timelion.rendered).toBe(true);
+            });
+        });
+
     });
-  });
 
 });
