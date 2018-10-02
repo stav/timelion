aedb116f45f330370998155f0b20f2c0ac30e99a
Steven Almeroth <sroth77@gmail.com>
2018-10-02 00:08:03 -0400

    add events with actual (hardcore) date processing

--- timetony/src/assets/js/entities/models.js
+++ timetony/src/assets/js/entities/models.js
@@ -1,10 +1,87 @@
+/**
+ * Class definitions
+ */
 "use strict"
+
+import calen from './calendar';
+import utils from './utils';
+
+const TODAY = new Date();
+
+/**
+ * Event parses dates from an object to calculate a start date & end date
+ */
+class Event
+{
+  /**
+   * Accept a an array of date triplets of the form [ year, month, day ]
+   */
+  constructor ( event )
+  {
+    this.sdate = new Date(...this._getStarTriplet( event.date ));
+    this.edate = new Date(...this._getEndrTriplet( event.date ));
+    this.title = event.title;
+    this._input = event;
+  }
+
+  /**
+   * Make sure we have an actual triplet (Array of length 3)
+   */
+  _getTriplet( triplet )
+  {
+    const _triplet = triplet || [];
+    const month = _triplet[1];
+    const year = _triplet[0];
+    const day = _triplet[2];
+    return [ year, month, day ]
+  }
+
+  /**
+   * Parse out our start date
+   */
+  _getStarTriplet( dates_as_pair_of_triplets )
+  {
+    const start_triplet = this._getTriplet( dates_as_pair_of_triplets[0] );
+    const month = start_triplet[1] || calen.JAN;  // One-based index: January = 1
+    const year = start_triplet[0] || TODAY.getFullYear();
+    const day = start_triplet[2] || 1;
+    return [ year, month - 1, day ]  // Convert month to zero-based index: January = 0
+  }
+
+  /**
+   * Parse out our end date
+   */
+  _getEndrTriplet( dates_as_pair_of_triplets )
+  {
+    const
+      start_triplet = this._getStarTriplet( dates_as_pair_of_triplets ),
+      start_date = dates_as_pair_of_triplets[0],
+      start_month = start_date && start_date[1],
+      start_year = start_date && start_date[0],
+      start_day = start_date && start_date[2],
+      end_date = dates_as_pair_of_triplets[1],
+      end_triplet = this._getTriplet( end_date ),
+      year = end_date ? (end_triplet[0]) : start_triplet[0],
+      month = end_date ? (end_triplet[1] || calen.DEC) : (start_month ? start_month : calen.DEC),  // One-based index: January = 1
+      last_day = new Date( year, month, 0).getDate(),  // month is used here as a zero-based index by not adding 1 with the day of 0
+      day = end_date ? (end_triplet[2] || last_day) : (start_day ? start_day : last_day);
+    return [ year, month - 1, day ]  // Convert month to zero-based index: January = 0
+  }
+
+}
+
 
 class Timelion
 {
   constructor ()
   {
+    this.unload()
+  }
+
+  unload ()
+  {
     this._loaded = false;
+    this.events = [];
   }
 
   loaded ()
@@ -16,19 +93,20 @@ class Timelion
   {
     const data = await http.get_json_data( filename );
     if ( data && data.events ){
-      console.log(data)
       this._loaded = true;
     }
   }
 
   async load_data ( data )
   {
-    if ( data && data.events ){
-      console.log(data)
-      this._loaded = true;
+    for ( const event of data.events ){
+      if ( !utils.isEmpty( event ))
+        this.events.push( new Event( event ))
     }
+    this._loaded = true;
   }
 
 }
+
 
 export default Timelion
---
+++ timetony/tests/Timelion.spec.js
@@ -0,0 +1,49 @@
+import Timelion from '../src/assets/js/entities/models';
+import utils    from '../src/assets/js/entities/utils';
+
+describe("Timelion instance", function() {
+
+    let timelion = new Timelion();
+    const data = {
+      "events": [
+        {"date": [[1950]], "title": "1950"},
+        {}
+      ]
+    }
+
+    beforeEach(() => {
+        timelion.unload();
+    });
+
+    it("should be an object", function() {
+        expect(typeof timelion).toEqual('object');
+    });
+
+    it("should not be loaded", function() {
+        expect(timelion.loaded()).toEqual(false);
+    });
+
+    it("should load some data", async function() {
+        await timelion.load_data(data)
+        expect(timelion.loaded()).toEqual(true);
+    });
+
+    it("should parse some data", async function() {
+        await timelion.load_data(data)
+        expect(timelion.events.length).toEqual(1);
+
+        const event = timelion.events[0];
+        expect(utils.type(event)).toEqual('Event');
+        expect(utils.isValidDate(event.sdate)).toBe(true);
+        expect(utils.isValidDate(event.edate)).toBe(true);
+
+        expect(event.sdate.getFullYear()).toBe(1950);
+        expect(event.sdate.getMonth()).toBe(0);
+        expect(event.sdate.getDate()).toBe(1);
+
+        expect(event.edate.getFullYear()).toBe(1950);
+        expect(event.edate.getMonth()).toBe(11);
+        expect(event.edate.getDate()).toBe(31);
+    });
+
+});
