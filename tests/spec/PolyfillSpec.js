854683b956526758e51b86fa70ae07301059a743
Steven Almeroth <sroth77@gmail.com>
2018-10-14 20:47:39 -0400

    remove old

--- tests/spec/PolyfillSpec.js
+++
@@ -1,7 +0,0 @@
-describe("polyfill", function() {
-
-  it("should expose the Date method: toLocaleFormat", function() {
-    expect( typeof (new Date()).toLocaleFormat ).toEqual('function');
-  });
-
-});
