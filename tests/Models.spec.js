f7da398c5ba2b77be905ccf85374c9160781a942
Steven Almeroth <sroth77@gmail.com>
2018-10-28 16:26:55 -0400

    remove files

--- tests/Models.spec.js
+++
@@ -1,9 +0,0 @@
-import Timelion from '../src/entities/models';
-
-describe("Timelion model", function() {
-
-    it("should be a class (function)", function() {
-        expect(typeof Timelion).toEqual('function');
-    });
-
-});
