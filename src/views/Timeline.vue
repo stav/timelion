295aa132e9fa48db16ce0ea6a5bef891cef838d7
Steven Almeroth <sroth77@gmail.com>
2018-10-28 13:12:14 -0400

    timelines display events

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -1,17 +1,29 @@
 <template>
-  <section>
-<!--
-    <p>({{ tid }})</p>
-    <p>({{ timelion.loaded }})</p>
-    <p>({{ timelion }})</p>
- -->
-  	<div id="timelion-years" ref="timelionYears">
+  <section id="timelion">
+    <div id="timelion-years" ref="timelionYears">
       <div class="year"
         v-for="year in years"
         :ref="`year_${year}`"
         :data-year="year"
         v-text="year"
       ></div>
+    </div>
+  	<div id="timelion-events" ref="timelionEvents">
+      <div class="event"
+        v-for="(event, i) in events"
+        :ref="`event_${event}`"
+        :title="event.title"
+        :tabindex="i+1"
+        :style="`marginLeft: ${event.offset.toFixed(2)}px`"
+        >
+        <div class="line"
+          :style="`width: ${event.width.toFixed(2)}px`"
+        ></div>
+        <span class="mark"
+          v-text="event.title"
+          :style="`left: -${event.width.toFixed(2)}px`"
+        ></span>
+      </div>
     </div>
 	</section>
 </template>
@@ -34,6 +46,7 @@
       // tid,
       timelines,
       timelion: new Timelion(),
+      events: [],
       years: [],
       // edata: timelines[ tid ],
 
@@ -47,15 +60,20 @@
 
     methods: {
       render () {
-        const $timelionYears = this.$refs.timelionYears;
-        $timelionYears.innerHTML = '';
 
+        // Reset data
         this.timelion.reset()
         this.timelion.load( this.edata )
+        this.$refs.timelionYears.innerHTML = '';
+        this.$refs.timelionEvents.innerHTML = '';
 
+        // Render years
         const size = this.timelion.final_year - this.timelion.first_year;
         const years = [...Array(size).keys()].map(i => i + this.timelion.first_year);
         this.years.push(...years)
+
+        // Render events
+        this.events.push(...this.timelion.events)
 
         console.log( this.tid, years, this.timelion.events.length, this.edata )
       },
@@ -83,6 +101,16 @@
 </script>
 
 <style>
+
+  #timelion{
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    overflow: auto;
+    -webkit-overflow-scrolling: touch;
+  }
 
   #timelion-years{
     position: absolute;
@@ -116,6 +144,53 @@
   #timelion-years .year span i{
     opacity: .5;
     font-style: normal;
+  }
+
+  #timelion-events{
+    padding-top: 40px;
+    padding-bottom: 5em;
+    position: relative;
+  }
+  #timelion-events:after{
+    content: '';
+    display: block;
+    clear: left;
+  }
+  #timelion .event{
+    padding-right: 20px;
+    padding-bottom: 5px;
+    vertical-align: middle;
+    white-space: nowrap;
+    float: left;
+    clear: left;
+  }
+  #timelion .event i{
+    color: rgba(255,255,255,.5);
+    margin-right: 3px;
+    padding-right: 7px;
+  }
+  #timelion .event .line{
+    display: inline-block;
+    overflow: hidden;
+    height: 0;
+    border: 4px solid #fff;
+    border-radius: 4px;
+    opacity: .3;
+    position: relative;
+  }
+  #timelion .event:hover .line{
+    opacity: .5;
+  }
+  #timelion .event:focus .line{
+    opacity: 1;
+  }
+  #timelion .event:focus .mark{
+    top: -9px;
+  }
+  #timelion .event .mark{
+    border-bottom: #777 solid 1px;
+    position: relative;
+    top: -8px;
   }
 
 </style>
